7f30ff41b0e3653aa6a089e989a52c6b4c2c3d91
aduose <aduose@msn.com>
2023-10-26 17:49:59 +0800

    Update blog config

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '宠物家', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '爱宠助手小佳', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '您身边的宠物管家', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://chongwujia.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '宠物, 宠物知识, 宠物寄养, 宠物健康', // 网站关键词 英文逗号隔开
