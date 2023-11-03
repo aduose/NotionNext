ee971d68e75fdc7976181fe5a7e301106d4c0c0f
aduose <aduose@msn.com>
2023-11-03 20:12:09 +0800

    Update logo description

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   LOGO_IMG: '/Logo.webp',
   TOP_BAR: true, // 显示顶栏
   TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>The enchanting world of Chinese folklore and mythology.</div>',
+  LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>Welcome to our website, where ancient Chinese tales come to life. Journey with us through the rich tapestry of China's history, as we delve into captivating stories from bygone eras. From legendary heroes to mythical creatures, from timeless romances to epic battles, our website is your portal to the enchanting world of Chinese folklore and mythology.</div>',
 
   AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
