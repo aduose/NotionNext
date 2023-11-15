6aaa46df7243abe9ac86f2b5571b38cca6db6753
aduose <aduose@msn.com>
2023-11-15 09:35:58 +0800

    Update config
    change public url

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'The Tales of China', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Your portal to the enchanting world of Chinese folklore and mythology.', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://chntales.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.talesofchina.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ancient Chinese tales,legendary heroes,mythical creatures,timeless romances,epic battles,Chinese folklore,Chinese mythology', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
