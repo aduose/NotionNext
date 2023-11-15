078f63ba253f40be6518b7b5f1ad1b1263226821
aduose <aduose@msn.com>
2023-11-15 09:40:36 +0800

    Update config

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'The Tales of China', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Tales of China', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Your portal to the enchanting world of Chinese folklore and mythology.', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.talesofchina.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ancient Chinese tales,legendary heroes,mythical creatures,timeless romances,epic battles,Chinese folklore,Chinese mythology', // 网站关键词 英文逗号隔开
